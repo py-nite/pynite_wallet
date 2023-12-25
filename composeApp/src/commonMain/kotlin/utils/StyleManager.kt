3242a42bb43249e28890e41de468d3ef46aed72d
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-12-25 01:19:24 -0300

    create stylemanager

---
+++ composeApp/src/commonMain/kotlin/utils/StyleManager.kt
@@ -0,0 +1,17 @@
+package utils
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+object Colors{
+    val primary = Color(0xFF000A0A)
+    val onPrimary = Color(0)
+    val secondary = Color(0xFFFB3C52)
+    val onSecondary = Color(0)
+    val tertiary = Color(0xFFFFFFFF)
+    val onTertiary = Color(0xFF9DEFEA)
+}
+
+object TextManager{
+    val smallWhite = TextStyle()
+}
